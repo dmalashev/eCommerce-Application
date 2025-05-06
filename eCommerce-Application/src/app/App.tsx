b1b43c8f9ceea55dc5e2d0e6b03f7de0cf3280e9
Shakhrii <sh.ramaldanova@gmail.com>
2025-05-06 23:53:28 +0300

    feat: add routes to app component

--- eCommerce-Application/src/app/App.tsx
+++ eCommerce-Application/src/app/App.tsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Login from '../pages/Login';
+import Registration from '../pages/Registration';
+import NotFound from '../pages/NotFound';
+import Catalog from '../pages/Catalog';
+
 export default function App() {
-  return <div>App component</div>;
+  return (
+    <div>
+      App component
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="registration" element={<Registration />}></Route>
+          <Route path="catalog" element={<Catalog />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }

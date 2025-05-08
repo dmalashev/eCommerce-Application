263a374bab30c416fa598ea4dac24f6e316e1eba
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-08 22:54:26 +0300

    feat: add example envipoment

---
+++ eCommerce-Application/src/api/client.ts
@@ -0,0 +1,10 @@
+
+
+
+export const projectKey = import.meta.env.VITE_API_URL;
+const clientId = import.meta.env.VITE_PROJECT_KEY
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET
+const authUrl = import.meta.env.VITE_AUTH_URL
+const apiUrl = import.meta.env.VITE_API_URL
+const scopes =import.meta.env.VITE_SCOPES
+

688038dcd4404aabce2e5b4cf1a3e1fa6fc3338e
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-16 19:13:03 +0300

    feat: move types enum to file type.d.ts

---
+++ eCommerce-Application/src/api/types-api.d.ts
@@ -0,0 +1,18 @@
+export enum ErrorsName {
+  BadRequest = 'BadRequest',
+}
+export enum BadRequest {
+  InvalidCustomerAccountCredentials = 'InvalidCustomerAccountCredentials',
+}
+
+export type CommercetoolsError = {
+  code?: string;
+  error?: { code: string; message: string }[];
+  message: string;
+  name: string;
+  statusCode: number;
+};
+type LoginRequest = Promise<{
+  customer: ClientResponse<CustomerPagedQueryResponse>;
+  token: TokenStore;
+}>;

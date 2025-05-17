18601cc3523f1d4806aada11c8bb12dabf5afdff
Dmitry Malashev <dmalashev@gmail.com>
2025-05-17 08:01:53 +0300

    feat: replace default header with header component from ant design

--- eCommerce-Application/src/components/header/Header.tsx
+++ eCommerce-Application/src/components/header/Header.tsx
@@ -1,31 +1,46 @@
 import { useNavigate } from 'react-router';
 import { PageRoutes } from '../../utils/page-routes';
+import { Layout, Menu } from 'antd';
 
-export default function Header() {
+const { Header } = Layout;
+
+export default function HeaderView() {
   const navigate = useNavigate();
+
+  const navItems: {
+    key: number;
+    label: string;
+  }[] = ['login', 'registration', 'catalog'].map((item, index) => ({
+    key: index,
+    label: item,
+  }));
+
   return (
-    <header style={{ display: 'flex', gap: '20px' }}>
-      <button
-        onClick={() => {
-          navigate(PageRoutes.LOGIN);
-        }}
-      >
-        Login
-      </button>
-      <button
-        onClick={() => {
-          navigate(PageRoutes.REGISTRATION);
-        }}
-      >
-        Registration
-      </button>
-      <button
-        onClick={() => {
-          navigate(PageRoutes.CATALOG);
-        }}
-      >
-        Catalog
-      </button>
-    </header>
+    // <header style={{ display: 'flex', gap: '20px' }}>
+    //   <button
+    //     onClick={() => {
+    //       navigate(PageRoutes.LOGIN);
+    //     }}
+    //   >
+    //     Login
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       navigate(PageRoutes.REGISTRATION);
+    //     }}
+    //   >
+    //     Registration
+    //   </button>
+    //   <button
+    //     onClick={() => {
+    //       navigate(PageRoutes.CATALOG);
+    //     }}
+    //   >
+    //     Catalog
+    //   </button>
+    // </header>
+    <Header>
+      <Menu items={navItems} mode="horizontal"></Menu>
+    </Header>
   );
 }

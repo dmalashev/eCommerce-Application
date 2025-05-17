fee5dbb15f8518972f514139ae43a17cf29fb1a7
Dmitry Malashev <dmalashev@gmail.com>
2025-05-17 10:11:46 +0300

    feat: add basic layout of the page

--- eCommerce-Application/src/app/App.tsx
+++ eCommerce-Application/src/app/App.tsx
@@ -5,20 +5,26 @@ import NotFound from '../pages/not-found/NotFound';
 import Catalog from '../pages/catalog/Catalog';
 import Header from '../components/header/Header';
 import { PageRoutes } from '../utils/page-routes';
+import { Layout } from 'antd';
+
+const { Content, Footer } = Layout;
 
 export default function App() {
   return (
-    <div id="root" style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Layout>
         <Header />
-        <Routes>
-          <Route path={PageRoutes.MAIN} element={<Catalog />}></Route>
-          <Route path={PageRoutes.LOGIN} element={<Login />}></Route>
-          <Route path={PageRoutes.REGISTRATION} element={<Registration />}></Route>
-          <Route path={PageRoutes.CATALOG} element={<Catalog />}></Route>
-          <Route path={PageRoutes.NOT_FOUND} element={<NotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <Content>
+          <Routes>
+            <Route path={PageRoutes.MAIN} element={<Catalog />}></Route>
+            <Route path={PageRoutes.LOGIN} element={<Login />}></Route>
+            <Route path={PageRoutes.REGISTRATION} element={<Registration />}></Route>
+            <Route path={PageRoutes.CATALOG} element={<Catalog />}></Route>
+            <Route path={PageRoutes.NOT_FOUND} element={<NotFound />}></Route>
+          </Routes>
+        </Content>
+        <Footer>The Records Store © 2025</Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }

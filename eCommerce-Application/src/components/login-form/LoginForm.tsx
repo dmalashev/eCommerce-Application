82738c6149274e94430fe2dd426fc5dbd53ff010
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-18 20:57:53 +0300

    refactor: add .env

--- eCommerce-Application/src/components/login-form/LoginForm.tsx
+++ eCommerce-Application/src/components/login-form/LoginForm.tsx
@@ -4,7 +4,6 @@ import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { PageRoutes } from '../../utils/page-routes';
 import { useNavigate } from 'react-router';
 import './login-form.css';
-import { createdCustomer } from '../../api/customer/create-customer';
 import { login } from '../../api/customer/autorizate-customer';
 import { CustomerDraft } from '@commercetools/platform-sdk';
 import { checkingError } from '../../api/handleError/checking-errors';

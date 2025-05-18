12f8226d40f90b76c9c85071411d06c73bfb3712
Dmitry Malashev <dmalashev@gmail.com>
2025-05-18 12:27:54 +0300

    feat: correct menu items

--- eCommerce-Application/src/components/header/Header.tsx
+++ eCommerce-Application/src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router';
 import { PageRoutes } from '../../utils/page-routes';
 import { Layout, Menu, Button, Flex } from 'antd';
@@ -6,51 +7,71 @@ import formatName from '../../utils/format-name';
 import Logo from '../logo/Logo';
 
 const { Header } = Layout;
-const noSelectedKey: number = -1;
+const noSelectedKey: string = '';
 
 export default function HeaderView() {
   const navigate = useNavigate();
   const location = useLocation();
+  const [menuKey, setMenuKey] = useState(0);
+
+  const refreshMenu = () => {
+    setMenuKey((previousKey) => previousKey + 1);
+  };
 
   const navItems: {
+    home: PageRoutes;
     catalog: PageRoutes;
   } = {
+    home: PageRoutes.MAIN,
     catalog: PageRoutes.CATALOG,
   };
 
   const currentPath: string = location.pathname;
 
-  const navPaths: PageRoutes[] = Object.values(navItems);
+  let selectedKey: string = noSelectedKey;
 
-  let selectedKey: number = noSelectedKey;
-
-  for (const [index, navPath] of navPaths.entries()) {
-    if (navPath === currentPath) {
-      selectedKey = index;
+  for (const [name, path] of Object.entries(navItems)) {
+    if (path === currentPath) {
+      selectedKey = `${name}${menuKey}`;
       break;
     }
   }
 
   const navItemObjects: {
-    key: number;
+    key: string;
     label: string;
     onClick: () => void | Promise<void>;
-  }[] = Object.entries(navItems).map(([name, path], index) => ({
-    key: index,
+  }[] = Object.entries(navItems).map(([name, path]) => ({
+    key: `${name}${menuKey}`,
     label: formatName(name),
     onClick: () => navigate(path),
   }));
+
+  const onClickLogin = () => {
+    refreshMenu();
+    navigate(PageRoutes.LOGIN);
+  };
+
+  const onClickRegistration = () => {
+    refreshMenu();
+    navigate(PageRoutes.REGISTRATION);
+  };
 
   return (
     <Header style={{ backgroundColor: 'white' }}>
       <Flex justify="space-between" align="center">
         <Logo />
-        <Menu items={navItemObjects} mode="horizontal" defaultSelectedKeys={[`${selectedKey}`]}></Menu>
+        <Menu
+          items={navItemObjects}
+          mode="horizontal"
+          defaultSelectedKeys={[selectedKey]}
+          disabledOverflow={true}
+        ></Menu>
         <Flex gap="small">
-          <Button type="default" icon={<LoginOutlined />} onClick={() => navigate(PageRoutes.LOGIN)}>
+          <Button type="default" icon={<LoginOutlined />} onClick={onClickLogin}>
             Log In
           </Button>
-          <Button type="primary" icon={<UserAddOutlined />} onClick={() => navigate(PageRoutes.REGISTRATION)}>
+          <Button type="primary" icon={<UserAddOutlined />} onClick={onClickRegistration}>
             Sign Up
           </Button>
         </Flex>

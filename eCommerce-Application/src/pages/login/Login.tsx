a951378b7efba16136b2b136637be6f47c0ed129
Dmitry Malashev <dmalashev@gmail.com>
2025-05-20 00:34:52 +0300

    fix: implement redirection to the main page when logged in and trying to access login and registration pages

--- eCommerce-Application/src/pages/login/Login.tsx
+++ eCommerce-Application/src/pages/login/Login.tsx
@@ -1,5 +1,15 @@
 import { LoginForm } from '../../components/login-form/LoginForm';
+import { useAuth } from '../../utils/hooks';
+import { useNavigate } from 'react-router';
+import { PageRoutes } from '../../utils/page-routes';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  if (auth.isLoggedIn) {
+    navigate(PageRoutes.MAIN);
+  }
+
   return <LoginForm />;
 }
--- eCommerce-Application/src/pages/registration/Registration.tsx
+++ eCommerce-Application/src/pages/registration/Registration.tsx
@@ -1,5 +1,15 @@
 import { RegistrationForm } from '../../components/registration-form/RegistrationForm';
+import { useAuth } from '../../utils/hooks';
+import { useNavigate } from 'react-router';
+import { PageRoutes } from '../../utils/page-routes';
 
 export default function Registration() {
+  const navigate = useNavigate();
+  const auth = useAuth();
+
+  if (auth.isLoggedIn) {
+    navigate(PageRoutes.MAIN);
+  }
+
   return <RegistrationForm />;
 }

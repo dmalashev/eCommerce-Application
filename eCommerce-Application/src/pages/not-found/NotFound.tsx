822cd084ad8677d53cdaefb7671ba423a0e0ef3c
Dmitry Malashev <dmalashev@gmail.com>
2025-05-19 13:45:38 +0300

    feat: add button for redirection to the home page

--- eCommerce-Application/src/pages/not-found/NotFound.tsx
+++ eCommerce-Application/src/pages/not-found/NotFound.tsx
@@ -1,15 +1,21 @@
 import { FrownOutlined } from '@ant-design/icons';
-import { Flex, Typography } from 'antd';
+import { Button, Flex, Typography } from 'antd';
+import { useNavigate } from 'react-router';
+import { PageRoutes } from '../../utils/page-routes';
 
 const { Title, Text } = Typography;
 
 export default function NotFound() {
+  const navigate = useNavigate();
   return (
-    <Flex vertical justify="center" align="center">
+    <Flex vertical justify="center" align="center" gap="middle">
       <Title level={2}>
         404 <FrownOutlined />
       </Title>
       <Text>This page doesn't exist</Text>
+      <Button type="primary" onClick={() => navigate(PageRoutes.MAIN)}>
+        Back to home page
+      </Button>
     </Flex>
   );
 }

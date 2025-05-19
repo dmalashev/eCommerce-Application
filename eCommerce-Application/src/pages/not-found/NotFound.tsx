d7aa13e3f343b3f22561c06bf6cc1dcc88bf5876
Dmitry Malashev <dmalashev@gmail.com>
2025-05-19 13:33:46 +0300

    feat: add content for the not-found page

--- eCommerce-Application/src/pages/not-found/NotFound.tsx
+++ eCommerce-Application/src/pages/not-found/NotFound.tsx
@@ -1,3 +1,15 @@
+import { FrownOutlined } from '@ant-design/icons';
+import { Flex, Typography } from 'antd';
+
+const { Title, Text } = Typography;
+
 export default function NotFound() {
-  return <div>NotFound page</div>;
+  return (
+    <Flex vertical justify="center" align="center">
+      <Title level={2}>
+        404 <FrownOutlined />
+      </Title>
+      <Text>This page doesn't exist</Text>
+    </Flex>
+  );
 }

81fe70faf0aff1638f7918045e577189da2022e2
Shakhrii <sh.ramaldanova@gmail.com>
2025-05-20 22:11:08 +0300

    fix: add redirect to main page after log in or sign up

--- eCommerce-Application/src/pages/login/Login.tsx
+++ eCommerce-Application/src/pages/login/Login.tsx
@@ -1,15 +1,10 @@
+import { Navigate } from 'react-router';
 import { LoginForm } from '../../components/login-form/LoginForm';
 import { useAuth } from '../../utils/hooks';
-import { useNavigate } from 'react-router';
 import { PageRoutes } from '../../utils/page-routes';
 
 export default function Login() {
-  const navigate = useNavigate();
   const auth = useAuth();
 
-  if (auth.isLoggedIn) {
-    navigate(PageRoutes.MAIN);
-  }
-
-  return <LoginForm />;
+  return auth.isLoggedIn ? <Navigate to={PageRoutes.MAIN} replace={true} /> : <LoginForm />;
 }
--- eCommerce-Application/src/pages/registration/Registration.tsx
+++ eCommerce-Application/src/pages/registration/Registration.tsx
@@ -1,15 +1,10 @@
 import { RegistrationForm } from '../../components/registration-form/RegistrationForm';
 import { useAuth } from '../../utils/hooks';
-import { useNavigate } from 'react-router';
+import { Navigate } from 'react-router';
 import { PageRoutes } from '../../utils/page-routes';
 
 export default function Registration() {
-  const navigate = useNavigate();
   const auth = useAuth();
 
-  if (auth.isLoggedIn) {
-    navigate(PageRoutes.MAIN);
-  }
-
-  return <RegistrationForm />;
+  return auth.isLoggedIn ? <Navigate to={PageRoutes.MAIN} replace={true} /> : <RegistrationForm />;
 }

2bf140265ff240ac6dc39dcfaaa854987b5af496
Dmitry Malashev <dmalashev@gmail.com>
2025-05-23 18:52:11 +0300

    feat: add style lint to vite-plugin-checker and add modern-normalize.css to stylelint ignore

--- eCommerce-Application/vite.config.ts
+++ eCommerce-Application/vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
         lintCommand: 'eslint "./src/**/*.{ts,tsx,js}"',
         useFlatConfig: true,
       },
+      stylelint: {
+        lintCommand: 'stylelint ./src/**/*.css',
+      },
     }),
   ],
   test: {

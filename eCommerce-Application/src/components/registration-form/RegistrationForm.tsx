1f1f7294eb11611abc3117962236c34becee4eed
Dmitry Malashev <dmalashev@gmail.com>
2025-05-24 17:51:15 +0300

    style: add empty lines for more readability in RegistrationForm.tsx

--- eCommerce-Application/src/components/registration-form/RegistrationForm.tsx
+++ eCommerce-Application/src/components/registration-form/RegistrationForm.tsx
@@ -97,18 +97,22 @@ export const RegistrationForm = () => {
 
   const onCountryChange = (addressType: AddressType, value: string) => {
     let pattern: RegExp;
-    let message = '';
+
+    let message: string = '';
+
     switch (value) {
       case 'USA': {
         pattern = new RegExp(PostalCodePattern.USA);
         message = PostalCodeMessages.USA;
         break;
       }
+
       case 'Russia': {
         pattern = new RegExp(PostalCodePattern.RUSSIA);
         message = PostalCodeMessages.Russia;
         break;
       }
+
       default: {
         pattern = new RegExp(PostalCodePattern.USA);
         message = PostalCodeMessages.USA;

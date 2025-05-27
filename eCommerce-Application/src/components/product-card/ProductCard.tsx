de8eaf86a1f9fcc4dd727a1260dc348284db8fe3
Dmitry Malashev <dmalashev@gmail.com>
2025-05-28 00:47:32 +0300

    feat: replace object with card content and add the content prop to ProductCard component

--- eCommerce-Application/src/components/product-card/ProductCard.tsx
+++ eCommerce-Application/src/components/product-card/ProductCard.tsx
@@ -1,45 +1,35 @@
 import { Card, Typography, Row, Space } from 'antd';
-import coverImg from '../../assets/temporary/David-bowie-lets-dance.jpg';
+import { ContentObject } from '../../assets/temporary/temporary';
 
 const { Title, Text } = Typography;
 
-// TODO: replace card obj with corresponding obj from API
-const card = {
-  cover: coverImg,
-  name: "Let's Dance",
-  author: 'David Bowie',
-  year: 1983,
-  price: 14.99,
-  discount: 0.3,
-};
-
-export default function ProductCard() {
+export default function ProductCard({ content }: { content: ContentObject }) {
   return (
-    <Card hoverable style={{ width: 300 }} cover={<img src={card.cover} alt="cover" />}>
+    <Card hoverable style={{ width: 300 }} cover={<img src={content.cover} alt="cover" />}>
       <Row>
-        <Title level={4} style={{ marginTop: 0 }}>
-          {card.name}
+        <Title level={4} ellipsis style={{ marginTop: 0 }} title={content.name}>
+          {content.name}
         </Title>
       </Row>
       <Row>
-        <Text>{card.author}</Text>
+        <Text ellipsis>{content.author}</Text>
       </Row>
       <Row>
-        <Text type="secondary">{card.year}</Text>
+        <Text type="secondary">{content.year}</Text>
       </Row>
       <Row style={{ justifySelf: 'end' }}>
         <Space size="small">
           <Text
             strong
-            delete={!!card.discount}
-            disabled={!!card.discount}
+            delete={!!content.discount}
+            disabled={!!content.discount}
             style={{ fontSize: 20 }}
-          >{`$${card.price}`}</Text>
-          {card.discount ? (
+          >{`$${content.price}`}</Text>
+          {content.discount ? (
             <Text
               strong
               style={{ fontSize: 20, color: '#db4444' }}
-            >{`$${(card.price * (1 - card.discount)).toFixed(2)}`}</Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
+            >{`$${(content.price * (1 - content.discount)).toFixed(2)}`}</Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
           ) : undefined}
         </Space>
       </Row>

0129d906d93df42c3f3243374c304183aad74c91
Dmitry Malashev <dmalashev@gmail.com>
2025-05-28 01:16:23 +0300

    feat: add link into the card

--- eCommerce-Application/src/components/product-card/ProductCard.tsx
+++ eCommerce-Application/src/components/product-card/ProductCard.tsx
@@ -1,38 +1,40 @@
 import { Card, Typography, Row, Space } from 'antd';
 import { ContentObject } from '../../assets/temporary/temporary';
 
-const { Title, Text } = Typography;
+const { Title, Text, Link } = Typography;
 
 export default function ProductCard({ content }: { content: ContentObject }) {
   return (
-    <Card hoverable style={{ width: 300 }} cover={<img src={content.cover} alt="cover" />}>
-      <Row>
-        <Title level={4} ellipsis style={{ marginTop: 0 }} title={content.name}>
-          {content.name}
-        </Title>
-      </Row>
-      <Row>
-        <Text ellipsis>{content.author}</Text>
-      </Row>
-      <Row>
-        <Text type="secondary">{content.year}</Text>
-      </Row>
-      <Row style={{ justifySelf: 'end' }}>
-        <Space size="small">
-          <Text
-            strong
-            delete={!!content.discount}
-            disabled={!!content.discount}
-            style={{ fontSize: 20 }}
-          >{`$${content.price}`}</Text>
-          {content.discount ? (
+    <Link href="/">
+      <Card hoverable style={{ width: 300 }} cover={<img src={content.cover} alt="cover" />}>
+        <Row>
+          <Title level={4} ellipsis style={{ marginTop: 0 }} title={content.name}>
+            {content.name}
+          </Title>
+        </Row>
+        <Row>
+          <Text ellipsis>{content.author}</Text>
+        </Row>
+        <Row>
+          <Text type="secondary">{content.year}</Text>
+        </Row>
+        <Row style={{ justifySelf: 'end' }}>
+          <Space size="small">
             <Text
               strong
-              style={{ fontSize: 20, color: '#db4444' }}
-            >{`$${(content.price * (1 - content.discount)).toFixed(2)}`}</Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
-          ) : undefined}
-        </Space>
-      </Row>
-    </Card>
+              delete={!!content.discount}
+              disabled={!!content.discount}
+              style={{ fontSize: 20 }}
+            >{`$${content.price}`}</Text>
+            {content.discount ? (
+              <Text
+                strong
+                style={{ fontSize: 20, color: '#db4444' }}
+              >{`$${(content.price * (1 - content.discount)).toFixed(2)}`}</Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
+            ) : undefined}
+          </Space>
+        </Row>
+      </Card>
+    </Link>
   );
 }

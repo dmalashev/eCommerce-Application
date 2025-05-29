de4aaf2df24dd7cb110772115430a3a62c6baaa1
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-29 21:20:25 +0300

    feat: first product

--- eCommerce-Application/src/main.tsx
+++ eCommerce-Application/src/main.tsx
@@ -2,6 +2,8 @@ import './assets/styles/modern-normalize.css';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import { AuthProvider } from './app/AuthProvider';
+import * as all from "../src/api/product/set-type-of-product"
+all
 
 
 createRoot(document.querySelector('body')!).render(

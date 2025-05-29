07adaa580e701cefcea6615372b736b85fb33cbd
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-30 01:55:58 +0300

    fix: dataProduct

--- eCommerce-Application/src/api/product/getProduct.ts
+++ eCommerce-Application/src/api/product/getProduct.ts
@@ -4,16 +4,13 @@ import {
   ApiRoot,
   ClientResponse,
   createApiBuilderFromCtpClient,
-  LocalizedString,
-  Image,
   ProductPagedQueryResponse,
   Product,
+  ProductData,
 } from '@commercetools/platform-sdk';
-type DataProduct = {
+export type DataProduct = {
   id: string;
-  name: string | LocalizedString;
-  description: string | LocalizedString;
-  images: Image[] | undefined;
+  current: ProductData;
 };
 export async function getProducts(): Promise<ProductPagedQueryResponse['results']> {
   const client: Client = clientBuilder
@@ -31,18 +28,13 @@ export async function getProducts(): Promise<ProductPagedQueryResponse['results'
     .execute();
   return response.body.results;
 }
-getProducts();
 export const dataProducts = async (): Promise<DataProduct[]> => {
   const array: ProductPagedQueryResponse['results'] = await getProducts();
   const data = array.map((item: Product) => {
     return {
       id: item.id,
-      name: item.masterData.current.name,
-      description: item.masterData.current.description || '',
-      images: item.masterData.current.masterVariant.images,
+      current: item.masterData.current,
     };
   });
-  console.log(data);
   return data;
 };
-dataProducts();

9ad88ad470a8f0b3218cff4b4c0cd49dfd06ee34
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-30 15:54:29 +0300

    feat: change categories of products

---
+++ eCommerce-Application/src/api/product/set-type-of-product.ts
@@ -0,0 +1,18 @@
+import { createApiBuilderFromCtpClient,  ProductDraft } from '@commercetools/platform-sdk';
+import { authMiddleware, client as clientBuilder, httpMiddleware, projectKey } from '../client/client';
+
+
+export async function setProductType(data:ProductDraft[]) {
+
+  const client = clientBuilder
+    .withProjectKey(projectKey)
+    .withClientCredentialsFlow(authMiddleware)
+    .withHttpMiddleware(httpMiddleware)
+    .build();
+  const apiRoot = createApiBuilderFromCtpClient(client);
+  data.forEach(async (product) => {
+    const response = await apiRoot.withProjectKey({ projectKey }).products().post({ body:product }).execute();
+    console.log(response.body,'response');
+
+  })
+}

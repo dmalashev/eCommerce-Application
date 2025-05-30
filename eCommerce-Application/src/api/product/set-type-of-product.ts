4863430e2c49ca3e43db8e6d48f2d89e7a562c42
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-30 15:56:35 +0300

    fix: add categories to products

--- eCommerce-Application/src/api/product/set-type-of-product.ts
+++ eCommerce-Application/src/api/product/set-type-of-product.ts
@@ -1,18 +1,56 @@
-import { createApiBuilderFromCtpClient,  ProductDraft } from '@commercetools/platform-sdk';
+import {
+  Attribute,
+  Category,
+  CategoryPagedQueryResponse,
+  ClientResponse,
+  createApiBuilderFromCtpClient,
+  ProductDraft,
+} from '@commercetools/platform-sdk';
 import { authMiddleware, client as clientBuilder, httpMiddleware, projectKey } from '../client/client';
 
-
-export async function setProductType(data:ProductDraft[]) {
-
+export async function setProductType(data: ProductDraft[]) {
   const client = clientBuilder
     .withProjectKey(projectKey)
     .withClientCredentialsFlow(authMiddleware)
     .withHttpMiddleware(httpMiddleware)
     .build();
   const apiRoot = createApiBuilderFromCtpClient(client);
-  data.forEach(async (product) => {
-    const response = await apiRoot.withProjectKey({ projectKey }).products().post({ body:product }).execute();
-    console.log(response.body,'response');
 
-  })
+  const categories: ClientResponse<CategoryPagedQueryResponse> = await apiRoot
+    .withProjectKey({ projectKey })
+    .categories()
+    .get({ queryArgs: { limit: 500 } })
+    .execute();
+
+  const catArray: Category[] = categories.body.results;
+  const catname = catArray.map((category: Category) => category.slug.en);
+
+  console.log(catname, 'slug');
+  data.forEach((product) => {
+    product.masterVariant?.attributes?.forEach((attribute: Attribute) => {
+      if (catname.includes(attribute.name)) {
+        const cat = catArray.find((category: Category) => category.slug.en === attribute.name);
+        product.categories?.push({
+          typeId: 'category',
+          id: cat?.id,
+        });
+      }
+      if (Array.isArray(attribute.value)) {
+        attribute.value.forEach((value) => {
+          if (catname.includes(value)) {
+            const cat = catArray.find((category: Category) => category.slug.en === value);
+            product.categories?.push({
+              typeId: 'category',
+              id: cat?.id,
+            });
+          }
+        });
+      }
+    });
+  });
+
+  data.forEach(async (product) => {
+    const response = await apiRoot.withProjectKey({ projectKey }).products().post({ body: product }).execute();
+    console.log(response.body, 'response');
+  });
 }

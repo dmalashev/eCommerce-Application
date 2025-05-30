33c91c42d526196383472b3e3e7d333919efc647
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-30 16:31:19 +0300

    fix: imports

--- eCommerce-Application/src/main.tsx
+++ eCommerce-Application/src/main.tsx
@@ -2,8 +2,7 @@ import './assets/styles/modern-normalize.css';
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import { AuthProvider } from './app/AuthProvider';
-import * as all from "../src/api/product/set-type-of-product"
-all
+
 
 
 createRoot(document.querySelector('body')!).render(

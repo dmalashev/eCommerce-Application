2ed12d2dbc6f3ece683f0cce26ec2c31edcb7e14
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-05-30 16:31:19 +0300

    fix: imports

--- eCommerce-Application/src/pages/catalog/Catalog.tsx
+++ eCommerce-Application/src/pages/catalog/Catalog.tsx
@@ -1,31 +1,9 @@
 import { useEffect, useState } from 'react';
-import {  dataProducts } from '../../api/product/getProduct';
+import { dataProducts } from '../../api/product/getProduct';
 import ProductCard from '../../components/product-card/ProductCard';
 import { DataProduct } from '../../api/types-api';
-// import coverImg1 from '../../assets/temporary/David-bowie-lets-dance.jpg';
-// import coverImg2 from '../../assets/temporary/m1000x1000.jfif';
-// import { ContentObject } from '../../assets/temporary/temporary';
-
-// // TODO: replace card objects with corresponding obj from API
-// const card1: ContentObject = {
-//   cover: coverImg1,
-//   name: "Let's Dance",
-//   author: 'David Bowie',
-//   year: 1983,
-//   price: 14.99,
-// };
-
-// const card2: ContentObject = {
-//   cover: coverImg2,
-//   name: 'Городские и цыганские романсы и несколько своих из концертов (В Клубе "пушкарев" 21 И 24 Января 2021 Live)',
-//   author: 'Пётр Налич, Иван Жук',
-//   year: 2022,
-//   price: 9.99,
-//   discount: 0.3,
-// };
 
 export default function Catalog() {
-
   const [content, setContent] = useState<DataProduct[]>([]);
   useEffect(() => {
     const fetchProducts = async () => {
@@ -47,7 +25,7 @@ export default function Catalog() {
       }}
     >
       {content.map((item) => (
-        <ProductCard key={item.id} content={item}  />
+        <ProductCard key={item.id} content={item} />
       ))}
     </div>
   );

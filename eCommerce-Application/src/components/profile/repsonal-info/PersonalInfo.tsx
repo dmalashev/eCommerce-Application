6ea1930b912d3681dac63de933d11cc2b76276c1
Shakhrii <sh.ramaldanova@gmail.com>
2025-06-02 00:22:57 +0300

    feat: add personal information component

--- eCommerce-Application/src/components/profile/repsonal-info/PersonalInfo.tsx
+++ eCommerce-Application/src/components/profile/repsonal-info/PersonalInfo.tsx
@@ -1,13 +1,22 @@
 import ProfileOutlined from '@ant-design/icons/lib/icons/ProfileOutlined';
-import { Typography } from 'antd';
+import { Flex, Typography } from 'antd';
 
 export const PersonalInfo = () => {
   return (
     <>
-      <Typography.Title level={5} style={{ margin: 0, color: '#DB4444' }}>
-        personal information
-      </Typography.Title>
-      <ProfileOutlined />
+      <Flex vertical>
+        <Flex justify="start" align="center" gap={10} style={{ marginBottom: '10px' }}>
+          <Typography.Title level={5} style={{ margin: 0, color: '#DB4444' }}>
+            personal information
+          </Typography.Title>
+          <ProfileOutlined style={{ width: '30px', height: '20px', fontSize: '20px', color: '#DB4444' }} />
+        </Flex>
+        <Typography.Text strong>Shakhri Ramaldanova</Typography.Text>
+        <Typography.Text type="secondary">ramaldanova_sh@mail.ru</Typography.Text>
+        <Typography.Text type="secondary" style={{ fontSize: '12px' }}>
+          07.10.1992
+        </Typography.Text>
+      </Flex>
     </>
   );
 };

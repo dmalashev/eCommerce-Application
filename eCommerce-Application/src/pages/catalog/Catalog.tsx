e20bbb5d739e5fe26d15233341a69f714fcfa4af
Dmitry Malashev <dmalashev@gmail.com>
2025-06-01 05:50:06 +0300

    feat: add tabs with media types to the catalog page

--- eCommerce-Application/src/pages/catalog/Catalog.tsx
+++ eCommerce-Application/src/pages/catalog/Catalog.tsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from 'react';
 import { dataProducts } from '../../api/product/getProduct';
 import ProductCard from '../../components/product-card/ProductCard';
-import { Layout, Flex } from 'antd';
+import { Layout, Flex, Segmented } from 'antd';
 import { cards } from '../../assets/temporary/cards'; // TODO: replace card objects with corresponding obj from API
+import { MediaTypes } from '../../types/enums';
+import { JSX } from 'react';
 
 const { Content } = Layout;
 
-export default function Catalog() {
-  const cardComponents = cards.map((card) => <ProductCard content={card} />);
+export default function Catalog(): JSX.Element {
+  const tabOptions: MediaTypes[] = [MediaTypes.VINYL, MediaTypes.CASSETTES, MediaTypes.CD];
+  const cardComponents: JSX.Element[] = cards.map((card) => <ProductCard content={card} />);
 
   return (
     <Layout>
       <Content style={{ padding: 24 }}>
-        <Flex gap="large" wrap justify="center">
-          {cardComponents}
+        <Flex vertical align="stretch" gap="40px">
+          <Segmented block options={tabOptions} size="large" style={{ fontWeight: 'bold' }} />
+          <Flex gap="large" wrap justify="center">
+            {cardComponents}
+          </Flex>
         </Flex>
       </Content>
     </Layout>

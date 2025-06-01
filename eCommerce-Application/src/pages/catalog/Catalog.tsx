bc172f035080579cd2b2a94f674ff536a5ac9403
Dmitry Malashev <dmalashev@gmail.com>
2025-06-01 06:48:45 +0300

    feat: add icons for tabs

--- eCommerce-Application/src/pages/catalog/Catalog.tsx
+++ eCommerce-Application/src/pages/catalog/Catalog.tsx
@@ -9,14 +9,46 @@ import { JSX } from 'react';
 const { Content } = Layout;
 
 export default function Catalog(): JSX.Element {
-  const tabOptions: MediaTypes[] = [MediaTypes.VINYL, MediaTypes.CASSETTES, MediaTypes.CD];
+  const tabsContent: {
+    imagePath: string;
+    tabName: MediaTypes;
+  }[] = [
+    {
+      imagePath: './src/assets/images/vinyl.png',
+      tabName: MediaTypes.VINYL,
+    },
+    {
+      imagePath: './src/assets/images/cassette.png',
+      tabName: MediaTypes.CASSETTES,
+    },
+    {
+      imagePath: './src/assets/images/cd.png',
+      tabName: MediaTypes.CD,
+    },
+  ];
+
+  const tabOptions: {
+    label: JSX.Element;
+    value: MediaTypes;
+  }[] = tabsContent.map((tab) => ({
+    label: (
+      <Flex vertical align="center" style={{ paddingTop: 8 }}>
+        <Flex justify="center" align="center" style={{ width: 50, height: 50 }}>
+          <img src={tab.imagePath} width="50px" />
+        </Flex>
+        <div>{tab.tabName}</div>
+      </Flex>
+    ),
+    value: tab.tabName,
+  }));
+
   const cardComponents: JSX.Element[] = cards.map((card) => <ProductCard content={card} />);
 
   return (
     <Layout>
       <Content style={{ padding: 24 }}>
         <Flex vertical align="stretch" gap="40px">
-          <Segmented block options={tabOptions} size="large" style={{ fontWeight: 'bold' }} />
+          <Segmented block options={tabOptions} />
           <Flex gap="large" wrap justify="center">
             {cardComponents}
           </Flex>

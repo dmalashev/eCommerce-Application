a450eaa3a99ce52ef16d30fc6d25a04aa1f5cbe7
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-01 12:04:57 +0300

    feat: Create functions for searching

--- eCommerce-Application/src/api/product/searchingProduct.ts
+++ eCommerce-Application/src/api/product/searchingProduct.ts
@@ -1,17 +1,19 @@
-import { apiRoot, projectKey } from "../client/client";
+import {  ProductProjection,  } from '@commercetools/platform-sdk';
+import { apiRoot, projectKey } from '../client/client';
 
+export async function searchingProducts(value: string): Promise<ProductProjection[]> {
+  const options  = {
+    queryArgs: {
+      'text.en': `${value}`, // поиск по названию или описанию
+      sort: 'price asc', // сортировка
+      limit: 20, // лимит на количество результатов
+      offset: 0, // сдвиг (для пагинации)
+      fuzzy: true, // нестрогий поиск
+    },
+  };
 
+  const response = await apiRoot.withProjectKey({ projectKey }).productProjections().search().get(options).execute();
 
-export async function getCategories(value:string) {
-
-  const response = await apiRoot
-    .withProjectKey({projectKey} )
-    .categories()
-     .withKey({ key: `cat-${value}` })
-  .get()
-    .execute();
   console.log(response.body);
-  return response.body;
-
+  return response.body.results;
 }
-// { queryArgs: { where:`slug(en:"${value}")` , limit: 500} }

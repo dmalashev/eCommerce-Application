b6534bec3337f747d472f287b147c454a8b72c7a
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-02 15:12:02 +0300

    feat: discount

--- eCommerce-Application/src/main.tsx
+++ eCommerce-Application/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import { AuthProvider } from './app/AuthProvider';
 
+
 createRoot(document.querySelector('body')!).render(
   <AuthProvider>
     <App />

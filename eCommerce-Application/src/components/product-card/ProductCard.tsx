73c446eba6386ed41d5323970be35a73ec9e8071
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-02 17:07:22 +0300

    feat: search product by category

--- eCommerce-Application/src/components/product-card/ProductCard.tsx
+++ eCommerce-Application/src/components/product-card/ProductCard.tsx
@@ -1,36 +1,38 @@
+import { ProductProjection } from '@commercetools/platform-sdk';
 import { Card, Typography, Row, Space } from 'antd';
-import { DataProduct } from '../../api/types-api';
 
 const { Title, Text, Link } = Typography;
 
-export default function ProductCard({ content }: { content: DataProduct }) {
+export default function ProductCard({ content }: { content: ProductProjection }) {
+  const card = {
+    name: content.name.en,
+    author: content.masterVariant.attributes?.find((attr) => attr.name === 'artist')?.value,
+    year: content.masterVariant.attributes?.find((attr) => attr.name === 'year')?.value,
+    cover: content.masterVariant.images?.[0].url,
+    discount: content.masterVariant.price?.discounted?.value.centAmount,
+    price: content.masterVariant.price?.value.centAmount,
+  };
   return (
     <Link href="/">
-      <Card hoverable style={{ width: 300 }} cover={<img src={content.current.masterVariant.images![0].url} alt="cover" />}>
+      <Card hoverable style={{ width: 300 }} cover={<img src={card.cover} alt="cover" />}>
         <Row>
-          <Title level={4} ellipsis style={{ marginTop: 0 }} title={content.current.name.en}>
-            {content.current.name.en}
+          <Title level={4} ellipsis style={{ marginTop: 0 }} title={card.name}>
+            {card.name}
           </Title>
         </Row>
         <Row>
-          <Text ellipsis>{content.current.masterVariant.attributes![0].value}</Text>
+          <Text ellipsis>{card.author}</Text>
         </Row>
         <Row>
-          <Text type="secondary">{content.current.masterVariant.attributes![0].value}</Text>
+          <Text type="secondary">{card.year}</Text>
         </Row>
         <Row style={{ justifySelf: 'end' }}>
           <Space size="small">
-            <Text
-              strong
-              delete={!!content.current.masterVariant.prices![0].value.centAmount}
-              disabled={!!content.current.masterVariant.prices![0].value.centAmount}
-              style={{ fontSize: 20 }}
-            >{`$${content.current.masterVariant.prices![0].value.centAmount / 100}`}</Text>
-            {content.current.masterVariant.prices![0].value.centAmount ? (
-              <Text
-                strong
-                style={{ fontSize: 20, color: '#db4444' }}
-              >{`$${(content.current.masterVariant.prices![0].value.centAmount / 100 * (1 - content.current.masterVariant.prices![0].value.centAmount)).toFixed(2)}`}</Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
+            <Text strong style={{ fontSize: 20 }}>{`$${card.price! / 100}`}</Text>
+            {card.discount ? (
+              <Text strong style={{ fontSize: 20, color: '#db4444' }}>
+                ${card.discount! / 100}
+              </Text> // logic of calculating a new price can be changed. This exp is only for demo purpose
             ) : undefined}
           </Space>
         </Row>

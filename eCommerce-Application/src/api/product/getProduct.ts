ec05b6b9f653e255cbe9be69738680682ee1465e
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-02 14:17:47 +0300

    fix:reset branch

---
+++ eCommerce-Application/src/api/product/getProduct.ts
@@ -0,0 +1,37 @@
+import { Client } from '@commercetools/ts-client';
+import { projectKey, client as clientBuilder, httpMiddleware, authMiddleware } from '../client/client';
+import {
+  ApiRoot,
+  ClientResponse,
+  createApiBuilderFromCtpClient,
+  ProductPagedQueryResponse,
+  Product,
+} from '@commercetools/platform-sdk';
+import { DataProduct } from '../types-api';
+
+export async function getProducts(): Promise<ProductPagedQueryResponse['results']> {
+  const client: Client = clientBuilder
+    .withProjectKey(projectKey)
+    .withClientCredentialsFlow(authMiddleware)
+    .withHttpMiddleware(httpMiddleware)
+    .build();
+
+  const apiRoot: ApiRoot = createApiBuilderFromCtpClient(client);
+
+  const response: ClientResponse<ProductPagedQueryResponse> = await apiRoot
+    .withProjectKey({ projectKey })
+    .products()
+    .get()
+    .execute();
+  return response.body.results;
+}
+export const dataProducts = async (): Promise<DataProduct[]> => {
+  const array: ProductPagedQueryResponse['results'] = await getProducts();
+  const data = array.map((item: Product) => {
+    return {
+      id: item.id,
+      current: item.masterData.current,
+    };
+  });
+  return data;
+};

b316a940c2fca5365f4d140ee0b9d6265ea57a61
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-09 17:36:00 +0300

    feat: created anonymous customer

--- eCommerce-Application/src/api/customer/anonymous-customer.ts
+++ eCommerce-Application/src/api/customer/anonymous-customer.ts
@@ -1,15 +1,41 @@
-import {
-  apiRootAnonymous,
-  apiRootCustomer,
-  projectKey,
-
-} from '../client/client';
+import { ApiRoot, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { authUrl, client, clientId, clientSecret, httpMiddleware, projectKey, scopes } from '../client/client';
+import { AuthMiddlewareOptions } from '@commercetools/ts-client';
 
 export async function createAnonymousCustomer() {
+  const getOrCreatedAnonymousId = () => {
+    let id = localStorage.getItem('anonymousId');
+    if (!id) {
+      id = crypto.randomUUID();
+      localStorage.setItem('anonymousId', id);
+    }
+    return id;
+  };
 
-  
-  const response = await apiRootAnonymous.withProjectKey({ projectKey }).apiClients().get().execute();
+  //Anonymous middleware
 
-  console.log(response  )
+  const anonymousMiddleware: AuthMiddlewareOptions = {
+    host: authUrl,
+    projectKey,
+    credentials: {
+      clientId: clientId,
+      clientSecret: clientSecret,
+      anonymousId: localStorage.getItem('anonymousId') || getOrCreatedAnonymousId(),
+    },
 
+    scopes: scopes.split(','),
+    httpClient: fetch,
+  };
+
+  const apiRootAnonymous: ApiRoot = createApiBuilderFromCtpClient(
+    client
+      .withProjectKey(projectKey)
+      .withAnonymousSessionFlow(anonymousMiddleware)
+      .withHttpMiddleware(httpMiddleware)
+      .build(),
+  );
+  apiRootAnonymous
+    .withProjectKey({ projectKey })
+    .carts()
+    .post({ body: { currency: 'USD', anonymousId: localStorage.getItem('anonymousId') || getOrCreatedAnonymousId() } });
 }

884bde872407f8399801e2724e8f477fa861a932
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-13 14:33:47 +0300

    feat: Recalculate Total Cost

--- eCommerce-Application/src/api/Cart/get.ts
+++ eCommerce-Application/src/api/Cart/get.ts
@@ -1,8 +1,8 @@
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { ApiRequest, Cart, ClientResponse, createApiBuilderFromCtpClient, LineItem, ProductProjection, ProductProjectionPagedSearchResponse } from '@commercetools/platform-sdk';
 import { client, httpMiddleware, projectKey } from '../client/client';
 
-export async function getCart() {
-  const isLogin = localStorage.getItem('access_token');
+export async function getCart(): Promise<Cart> {
+  const isLogin: boolean = !!localStorage.getItem('access_token');
   if (isLogin) {
     const apiRootCustomer = createApiBuilderFromCtpClient(
       client.withProjectKey(projectKey).withHttpMiddleware(httpMiddleware).build(),
@@ -25,4 +25,30 @@ export async function getCart() {
     console.log('anonymous customer cart', response.body);
     return response.body;
   }
+}
+
+
+export async function getCartProducts(): Promise<ProductProjection[]> {
+  const items: LineItem[] = (await getCart()).lineItems;
+  const apiRootCustomer = createApiBuilderFromCtpClient(
+    client.withProjectKey(projectKey).withHttpMiddleware(httpMiddleware).build(),
+  );
+
+  const responseProducts: ClientResponse<ProductProjectionPagedSearchResponse> = await apiRootCustomer
+    .withProjectKey({ projectKey })
+    .productProjections()
+    .search()
+    .get({
+      queryArgs: {
+        markMatchingVariants: true,
+        filter: items.map((item) => item.id),
+      },
+    })
+    .execute();
+  return responseProducts.body.results;
+}
+
+export async function getTotalCost() {
+  const cart = await getCart();
+  return cart.totalPrice.centAmount / 100;
 }

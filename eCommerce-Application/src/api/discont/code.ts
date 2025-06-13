a81dca89dc5d57b3967ba3489ddbe34dcb9b364c
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-13 16:29:50 +0300

    feat: discount

---
+++ eCommerce-Application/src/api/discont/code.ts
@@ -0,0 +1,19 @@
+import {  DiscountCodeDraft } from "@commercetools/platform-sdk";
+import { apiRoot, projectKey } from "../client/client";
+
+export async function createdCodeDiscount(cartDiscountId:string, code:string) {
+  const response = await apiRoot.withProjectKey({ projectKey }).cartDiscounts().get().execute();
+
+  const discountCodeDraft:DiscountCodeDraft = {
+    code: code,
+    cartDiscounts: [
+      {
+        typeId: 'cart-discount',
+        id: cartDiscountId,
+      },
+    ],
+    isActive: true,
+  };
+  const createResponse = await apiRoot.withProjectKey({ projectKey }).discountCodes().post({ body: discountCodeDraft }).execute();
+  return createResponse.body;
+}

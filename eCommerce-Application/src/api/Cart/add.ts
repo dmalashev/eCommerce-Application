20cf883cdbb49942293ea1d801538c4e1a06ddc0
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-13 15:01:02 +0300

    feat: Apply Promo Code

--- eCommerce-Application/src/api/Cart/add.ts
+++ eCommerce-Application/src/api/Cart/add.ts
@@ -30,6 +30,7 @@ export async function addItemToCart(product: ProductProjection, quantity: number
       .get()
       .execute();
 
+    console.log(product);
     await apiRootCustomer
       .withProjectKey({ projectKey })
       .me()

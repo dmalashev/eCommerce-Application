da9e2b672c4787185ca55ea47ce16b7c6c358924
Dmitry Malashev <dmalashev@gmail.com>
2025-06-17 08:14:02 +0300

    feat: add photos and short bios of shakhri and artyom

--- eCommerce-Application/src/components/about-us-card/AboutUsCard.tsx
+++ eCommerce-Application/src/components/about-us-card/AboutUsCard.tsx
@@ -1,5 +1,21 @@
-import { Card } from 'antd';
+import { Flex, Typography } from 'antd';
 
-export default function AboutUsCard() {
-  return <Card></Card>;
+type AboutUsCard = {
+  photo: string;
+  name: string;
+  description: string;
+};
+
+const { Title, Text } = Typography;
+
+export default function AboutUsCard({ photo, name, description }: AboutUsCard) {
+  return (
+    <Flex>
+      <img alt="photo" src={photo} />
+      <Flex>
+        <Title level={2}>{name}</Title>
+        <Text>{description}</Text>
+      </Flex>
+    </Flex>
+  );
 }
--- eCommerce-Application/src/pages/about-us/AboutUs.tsx
+++ eCommerce-Application/src/pages/about-us/AboutUs.tsx
@@ -1,5 +1,7 @@
 import { Typography } from 'antd';
 import AboutUsCard from '../../components/about-us-card/AboutUsCard';
+import artyomPhoto from '../../assets/images/developers-photos/artyom.jpg';
+import shakhriPhoto from '../../assets/images/developers-photos/shakhri.jpg';
 
 const { Title } = Typography;
 
@@ -7,9 +9,32 @@ export default function AboutUs() {
   return (
     <>
       <Title>About Us</Title>
-      <AboutUsCard />
-      <AboutUsCard />
-      <AboutUsCard />
+      <AboutUsCard
+        photo={artyomPhoto}
+        name="Artyom Migalev"
+        description="I am a frontend developer with experience in creating modern, responsive and interactive web applications. I specialize in JavaScript, HTML, CSS, TypeScript. My goal is to create convenient, productive and visually attractive interfaces that improve user experience.
+In my work, I pay attention to code cleanliness, performance optimization and cross-browser compatibility. I constantly study new technologies and tools to stay up to date with the latest trends in web development.
+
+Skills:
+
+JavaScript (ES6+)
+
+HTML5, CSS3, SASS/SCSS
+
+Build tools (Webpack, Vite)
+
+Version control systems (Git, GitHub)
+
+Cross-browser and responsive layout
+
+Basic knowledge of TypeScript"
+      />
+      <AboutUsCard
+        photo={shakhriPhoto}
+        name="Shakhperi Ramaldanova"
+        description="I would like to be a front-end developer and work for a big company. I've had experience in developing. I used to develop android applications and now I am more interested in web developing"
+      />
+      {/* <AboutUsCard /> */}
     </>
   );
 }

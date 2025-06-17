35bd58dca0a2b0e87c44890552de306f32107900
Dmitry Malashev <dmalashev@gmail.com>
2025-06-17 09:25:12 +0300

    feat: add contributions for shakhri and artyom

--- eCommerce-Application/src/components/about-us-card/AboutUsCard.tsx
+++ eCommerce-Application/src/components/about-us-card/AboutUsCard.tsx
@@ -1,21 +1,29 @@
 import { Flex, Typography } from 'antd';
+import './about-us-card.css';
 
 type AboutUsCard = {
   photo: string;
   name: string;
   description: string;
+  contribution: string[];
 };
 
 const { Title, Text } = Typography;
 
-export default function AboutUsCard({ photo, name, description }: AboutUsCard) {
+export default function AboutUsCard({ photo, name, description, contribution }: AboutUsCard) {
+  const contributionItems = contribution.map((item, index) => <li key={index}>{item}</li>);
+
   return (
-    <Flex>
-      <img alt="photo" src={photo} />
-      <Flex>
+    <div className="about-us-card-container">
+      <div className="about-us-photo-container">
+        <img alt="photo" src={photo} width="500" />
+      </div>
+
+      <div className="about-us-card-description-container">
         <Title level={2}>{name}</Title>
         <Text>{description}</Text>
-      </Flex>
-    </Flex>
+        <ul>{contributionItems}</ul>
+      </div>
+    </div>
   );
 }
--- eCommerce-Application/src/pages/about-us/AboutUs.tsx
+++ eCommerce-Application/src/pages/about-us/AboutUs.tsx
@@ -12,27 +12,14 @@ export default function AboutUs() {
       <AboutUsCard
         photo={artyomPhoto}
         name="Artyom Migalev"
-        description="I am a frontend developer with experience in creating modern, responsive and interactive web applications. I specialize in JavaScript, HTML, CSS, TypeScript. My goal is to create convenient, productive and visually attractive interfaces that improve user experience.
-In my work, I pay attention to code cleanliness, performance optimization and cross-browser compatibility. I constantly study new technologies and tools to stay up to date with the latest trends in web development.
-
-Skills:
-
-JavaScript (ES6+)
-
-HTML5, CSS3, SASS/SCSS
-
-Build tools (Webpack, Vite)
-
-Version control systems (Git, GitHub)
-
-Cross-browser and responsive layout
-
-Basic knowledge of TypeScript"
+        description="I am a frontend developer with experience in creating modern, responsive and interactive web applications. I specialize in JavaScript, HTML, CSS, TypeScript. My goal is to create convenient, productive and visually attractive interfaces that improve user experience. In my work, I pay attention to code cleanliness, performance optimization and cross-browser compatibility. I constantly study new technologies and tools to stay up to date with the latest trends in web development."
+        contribution={['Set up CommerceTools', 'ALL of the API functions(!)', 'Make product list', 'Unit tests']}
       />
       <AboutUsCard
         photo={shakhriPhoto}
         name="Shakhperi Ramaldanova"
         description="I would like to be a front-end developer and work for a big company. I've had experience in developing. I used to develop android applications and now I am more interested in web developing"
+        contribution={['Login page', 'Registration page', 'User profile page', 'Cart page', 'Main page', 'UI tests']}
       />
       {/* <AboutUsCard /> */}
     </>

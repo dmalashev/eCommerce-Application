adc8c3792c378a42351523194fc79d4bb3bdc5ed
Dmitry Malashev <dmalashev@gmail.com>
2025-06-17 07:08:57 +0300

    feat: add template for a card

---
+++ eCommerce-Application/src/components/about-us-card/AboutUsCard.tsx
@@ -0,0 +1,5 @@
+import { Card } from 'antd';
+
+export default function AboutUsCard() {
+  return <Card></Card>;
+}
--- eCommerce-Application/src/pages/about-us/AboutUs.tsx
+++ eCommerce-Application/src/pages/about-us/AboutUs.tsx
@@ -1,3 +1,15 @@
+import { Typography } from 'antd';
+import AboutUsCard from '../../components/about-us-card/AboutUsCard';
+
+const { Title } = Typography;
+
 export default function AboutUs() {
-  return <div>About Us</div>;
+  return (
+    <>
+      <Title>About Us</Title>
+      <AboutUsCard />
+      <AboutUsCard />
+      <AboutUsCard />
+    </>
+  );
 }

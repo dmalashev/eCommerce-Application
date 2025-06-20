99893eb3cbf86023dd8d55f391ebe10ca2be8019
Artyom Migalev <50071722+artmigalev@users.noreply.github.com>
2025-06-20 19:59:07 +0300

    feat:  reloaded if not token

--- eCommerce-Application/src/api/client/client.test.ts
+++ eCommerce-Application/src/api/client/client.test.ts
@@ -1,6 +1,12 @@
 import { HttpMiddlewareOptions } from '@commercetools/ts-client';
 import { client } from './client';
-import { test, expect, describe } from 'vitest';
+import { test } from 'vitest';
+import { reloaded } from './client';
+import { logout } from '../customer/logout';
+import { createAnonymousCustomer } from '../customer/anonymous-customer';
+import { StorageTokenKeys } from '../../types/enums';
+import { vi } from 'vitest';
+
 
 const httpMiddleware: HttpMiddlewareOptions = {
   host: 'http://localhost:3000',
@@ -8,6 +14,40 @@ const httpMiddleware: HttpMiddlewareOptions = {
   includeRequestInErrorResponse: true,
   httpClient: fetch,
 };
+
+
+vi.mock('../customer/logout', () => ({
+  logout: vi.fn(),
+}));
+
+vi.mock('../customer/anonymous-customer', () => ({
+  createAnonymousCustomer: vi.fn(),
+}));
+
+describe('reloaded()', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    vi.clearAllMocks();
+  });
+
+  it('should logout и createAnonymousCustomer, if not token', async () => {
+    await reloaded();
+
+    expect(logout).toHaveBeenCalledTimes(1);
+    expect(createAnonymousCustomer).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not logout и createAnonymousCustomer, if token exists', async () => {
+    localStorage.setItem(StorageTokenKeys.ACCESS_TOKEN, 'fake-token');
+
+    await reloaded();
+
+    expect(logout).not.toHaveBeenCalled();
+    expect(createAnonymousCustomer).not.toHaveBeenCalled();
+  });
+});
+
+
 
 describe('client', () => {
   test('should build client with no errors', () => {
